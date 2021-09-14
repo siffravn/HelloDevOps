5351e772d70d66b8cc189d745e4ed6d55dd7cfcf
Siff Ravn <siff.ravn@gmail.com>
2021-09-14 10:42:45 +0200

    L03-2.6-7: feat(routing) - add switch routing

--- web/src/index.js
+++ web/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter, Route} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 import About from "./About";
 
 ReactDOM.render(
   <React.StrictMode>
       <HashRouter>
-          <Route path={"/"} exact component={App}/>
-          <Route path={"/about"} component={About}/>
+          <Switch>
+              <Route path={"/"} exact component={App}/>
+              <Route path={"/about"} component={About}/>
+          </Switch>
       </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
