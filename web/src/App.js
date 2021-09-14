711b668563a835525ae3416e9f88cfae7f626d0a
Siff Ravn <siff.ravn@gmail.com>
2021-09-14 10:00:05 +0200

    L03-1.4+1.5: feat(app) - add Responsive Layout w. Grid

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {TextField, Button} from "@material-ui/core";
+import {TextField, Button, Grid} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined"></TextField>
-        </form>
-        <Button variant="contained" color="primary">Primary</Button>
+        <Grid
+            container
+            spacing={4}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+        >
+           <form>
+             <TextField id="outlined-basic" label="Outlined" variant="outlined"></TextField>
+           </form>
+           <Button variant="contained" color="primary">Primary</Button>
+        </Grid>
       </header>
     </div>
   );
