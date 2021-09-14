dcc618af0db065fa7d4bf94d47189cac0f49377f
Siff Ravn <siff.ravn@gmail.com>
2021-09-14 09:33:30 +0200

    L03-1.3: feat(app) - add TextField and Button

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {TextField, Button} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined"></TextField>
+        </form>
+        <Button variant="contained" color="primary">Primary</Button>
       </header>
     </div>
   );
