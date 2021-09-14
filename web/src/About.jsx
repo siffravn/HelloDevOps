cc7fb91e90cb7bcb30aa2bbf28027419a2424ca6
Siff Ravn <siff.ravn@gmail.com>
2021-09-14 11:12:43 +0200

    L03-2.14-5: feat(routing) - add browser history manipulation

--- web/src/About.jsx
+++ web/src/About.jsx
@@ -1,11 +1,15 @@
-const About = ({match})=>{
-    console.log(match);
+import {withRouter} from "react-router-dom";
+import {Button} from "@material-ui/core";
 
+const About = withRouter(({history,match})=>{
+    console.log(history);
+    console.log(match);
     return (
         <div>
             <h1>About {match.params.text}</h1>
+            <Button variant="contained" color={"primary"} onClick={()=>history.push("/")}>Go to front</Button>
         </div>
         )
-};
+});
 
 export default About;
