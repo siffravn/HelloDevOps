61c74f1d32b69933b159bef305353fe9f2ed2d77
Siff Ravn <siff.ravn@gmail.com>
2021-09-21 10:18:18 +0200

    L03-3.8-10: feat(state) - just some styling

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {TextField, Button, Grid} from "@material-ui/core"
+import {TextField, Button, Grid, Container, Box} from "@material-ui/core"
 import GiraffeStore from "./GiraffeStore";
 import {observer} from "mobx-react-lite";
 
@@ -10,31 +10,34 @@ const giraffeStore = new GiraffeStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Grid
-            container
-            spacing={4}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-        >
-           <form>
-             <TextField id="outlined-basic" label="Outlined" variant="outlined"></TextField>
-           </form>
-           <Button
-               variant="contained"
-               color="primary"
-               onClick={()=>giraffeStore.giraffes.push("Elmer")}
-           >Tilføj Giraf</Button>
-        </Grid>
-          <ul>
-              {giraffeStore.giraffes.map((giraffeName,key)=>
-                  <li key={key}>{giraffeName}</li>
-              )}
-          </ul>
-      </header>
-    </div>
+    <Container className="App" p>
+        <header className="App-header">
+            <Box sx={{pt: 16}}>
+                <Grid
+                    container
+                    spacing={4}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <form>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined"></TextField>
+                    </form>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={()=>giraffeStore.giraffes.push("Elmer")}
+                    >Tilføj Giraf</Button>
+                </Grid>
+                <ul>
+                    {giraffeStore.giraffes.map((giraffeName,key)=>
+                        <li key={key}>{giraffeName}</li>
+                    )}
+                </ul>
+
+            </Box>
+        </header>
+    </Container>
   );
 }
 
