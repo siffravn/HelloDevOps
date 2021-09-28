c22f65d923d1bad28e132454f12dead7d26d511e
Anders Brandt <Anders@brandt-it.dk>
2021-09-28 11:46:04 +0200

    render quizcompletion

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,17 +2,19 @@ import './App.css';
 import BasicLayout from './components/layouts/basicLayout';
 import QuizComponent from './components/quiz/quizComponent';
 import QuizSelection from './components/quiz/quizSelection';
-import { QuizStore } from './stores/quizStore';
+import QuizCompletion from './components/quiz/quizCompletion';
+import {QuizStore} from './stores/quizStore';
 
 function App() {
 
-  return (
-    <BasicLayout>
-      {/* router output here. For now, render quizView below */}
-      <QuizSelection quizStore={QuizStore} />
-      <QuizComponent />
-    </BasicLayout>
-  );
+    return (
+        <BasicLayout>
+            {/* router output here. For now, render quizView below */}
+            <QuizCompletion correctAnswers={3} questionAmount={10}/>
+            <QuizSelection quizStore={QuizStore}/>
+            <QuizComponent/>
+        </BasicLayout>
+    );
 }
 
 export default App;
