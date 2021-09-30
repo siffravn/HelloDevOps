fe36672143c4c143ffd73822efd150b17185bb39
Anders Brandt <Anders@brandt-it.dk>
2021-09-30 15:59:38 +0200

    fix hashrouting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,24 @@
 import './App.css';
 import QuizSelection from './components/quiz/QuizSelection/quizSelection';
-import { QuizStore } from './stores/quizStore';
-import { FC } from 'react';
-import  QuizPage from './pages/QuizPage'
-import  NotFoundPage from './pages/NotFoundPage'
-import  HomePage from './pages/HomePage'
-import { Route, Switch } from "react-router-dom";
+import {QuizStore} from './stores/quizStore';
+import {FC} from 'react';
+import QuizPage from './pages/QuizPage'
+import NotFoundPage from './pages/NotFoundPage'
+import HomePage from './pages/HomePage'
+import {HashRouter, Route, Switch} from "react-router-dom";
 
 const App: FC = () => {
-  document.title = "YouQuiz"
-  return (
-    <Switch>
-     <Route exact path="/" component={HomePage} />
-     <Route exact path="/quiz" component={QuizPage} />
-     <Route exact path="/quiz/:quizId" component={QuizPage} />
-     <Route exact component={NotFoundPage}></Route>
-    </Switch>
-  );
+    document.title = "YouQuiz"
+    return (
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/quiz" component={QuizPage}/>
+                <Route exact path="/quiz/:quizId" component={QuizPage}/>
+                <Route exact component={NotFoundPage}></Route>
+            </Switch>
+        </HashRouter>
+    );
 }
 
 export default App;
