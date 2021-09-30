789090fb71a6faf156633b00331ed657c23d4824
Anders Brandt <Anders@brandt-it.dk>
2021-09-30 12:47:26 +0200

    add navigation to quizCompletionPage

--- web/src/components/quiz/quizCompletion.tsx
+++ web/src/components/quiz/quizCompletion.tsx
@@ -1,19 +1,21 @@
 import {Box, HStack, Text, VStack, Button, Stack, Center} from '@chakra-ui/react';
 import {FC} from 'react';
+import {Link} from "react-router-dom";
 
 interface completionProp {
     correctAnswers: number,
-    questionAmount: number
+    questionAmount: number,
+    quizName?: string
 }
 
-const QuizCompletion: FC<completionProp> = ({correctAnswers, questionAmount}) => {
+const QuizCompletion: FC<completionProp> = ({correctAnswers, questionAmount, quizName}) => {
     return (
         <Box>
-            <Text align={"center"}>10 gode spørgsmål om DevOps</Text>
+            <Text align={"center"}>{quizName}</Text>
             <Text mt={75} align={"center"}>Tillykke du er færdig</Text>
             <Text mt={100} align={"center"}>{correctAnswers}/{questionAmount} korrekte svar</Text>
             <Center mt={150}>
-                <Button>Afslut Quiz</Button>
+                <Link to={"/"}><Button>Afslut Quiz</Button></Link>
             </Center>
         </Box>
     )
