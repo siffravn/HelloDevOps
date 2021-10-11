696de9f82bc4a62677b61b1cd2e835900a398db8
Siff Ravn <siff.ravn@gmail.com>
2021-10-11 15:48:26 +0200

    L05-1.4: test(app)

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {HashRouter} from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('name', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<HashRouter> <App/> </HashRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
