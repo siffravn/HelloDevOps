c9504fa029f7ca388d8db5e8a0bc81835d201b18
Siff Ravn <siff.ravn@gmail.com>
2021-10-12 11:30:13 +0200

    L05-1.9: FE test(app) - add elmer on click

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 import {sillyMethod} from './App'
 import {HashRouter} from "react-router-dom";
@@ -13,3 +13,14 @@ test('Silly test',()=>{
   const result = sillyMethod(2,3)
   expect(result).toBe(5);
 })
+
+test('adds giraffe on click', async () => {
+  render(<App/>)
+
+  // Click button
+  fireEvent.click(screen.getByText('Tilføj Giraf'))
+
+  // Wait for page to update with query text
+  const items = await screen.findAllByText('Elmer')
+  expect(items).toHaveLength(1)
+});
