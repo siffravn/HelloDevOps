84ff806a8cbf0700fecf74c4cd736c0794b09824
Siff Ravn <siff.ravn@gmail.com>
2021-10-12 11:01:16 +0200

    L05-1.8: FE test(app) - rewrite to react-testing-library

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import {sillyMethod} from './App'
 import {HashRouter} from "react-router-dom";
 
-it('name', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HashRouter> <App/> </HashRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('app', () => {
+  render(<HashRouter> <App/> </HashRouter>)
+  expect(screen.getByText('Tilføj Giraf')).toBeInTheDocument();
 })
 
 test('Silly test',()=>{
