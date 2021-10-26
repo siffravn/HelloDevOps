60dbbfb503cc9f48150bb98c4df4c605cb7a6e5b
Siff Ravn <siff.ravn@gmail.com>
2021-10-26 19:13:30 +0200

    L06-4: BE feat(giraffes) - update GiraffeService to use GiraffeDAO
    - tests are temporarily disabled till mocking is implemented

--- src/main/java/GiraffeService.java
+++ src/main/java/GiraffeService.java
@@ -1,17 +1,26 @@
+import giraffes.Giraffe;
+import giraffes.GiraffeDAO;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
 import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("giraffes")
 public class GiraffeService {
-    List<String> giraffes = Arrays.asList("Melman", "Elmer");
+
+    GiraffeDAO giraffeDAO = new GiraffeDAO();
 
     @GET
-    public List<String> getGiraffes(){
-        return giraffes;
+    public List<Giraffe> getGiraffes(){
+        return giraffeDAO.getAll();
+    }
+
+    @POST
+    public Giraffe save(Giraffe giraffe){
+        return giraffeDAO.save(giraffe);
     }
 }
