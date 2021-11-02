583d29b8f928b6b2e77f24b0dc62263a9522bc9c
Anders Pedersen <afck97@hotmail.com>
2021-11-02 11:42:46 +0100

    added form library and form submit for login

--- web/src/components/login/LoginForm.tsx
+++ web/src/components/login/LoginForm.tsx
@@ -1,24 +1,94 @@
-import { Button } from "@chakra-ui/button";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Input } from "@chakra-ui/input";
-import { Stack } from "@chakra-ui/layout";
-import { FC } from "react";
-import { PasswordField } from "./PasswordField";
+import { Button, IconButton } from "@chakra-ui/button";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { Box, Flex, Stack } from "@chakra-ui/layout";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useColorModeValue as mode } from "@chakra-ui/react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
 
 const LoginForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    const { email, password } = data;
+
+    //TODO handle login logic
+    console.log(email, password);
+  };
+
+  const [revealPassword, setRevealPassword] = useState(false);
+
+  const onClickReveal = () => {
+    setRevealPassword((revealPassword) => !revealPassword);
+  };
+
   return (
-    // this should be wrapped in some sort of form element and login should be
-    // handled in the onSubmit method
-    <Stack spacing="6">
-      <FormControl id="email">
-        <FormLabel>Email address</FormLabel>
-        <Input name="email" type="email" autoComplete="email" required />
-      </FormControl>
-      <PasswordField />
-      <Button type="submit" colorScheme="blue" size="lg" fontSize="md">
-        Sign in
-      </Button>
-    </Stack>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing="6">
+        <FormControl isInvalid={errors.email}>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input
+            id="email"
+            type="email"
+            autoComplete="email"
+            {...register("email", { required: true })}
+          />
+          <FormErrorMessage>
+            {errors.email && errors.email.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={errors.password}>
+          <Flex justify="space-between">
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Box
+              as="a"
+              color={mode("blue.600", "blue.200")}
+              fontWeight="semibold"
+              fontSize="sm"
+            >
+              Forgot Password?
+            </Box>
+          </Flex>
+          <InputGroup>
+            <InputRightElement>
+              <IconButton
+                bg="transparent !important"
+                variant="ghost"
+                aria-label={
+                  revealPassword ? "Mask password" : "Reveal password"
+                }
+                icon={revealPassword ? <HiEyeOff /> : <HiEye />}
+                onClick={onClickReveal}
+              />
+            </InputRightElement>
+            <Input
+              id="password"
+              type={revealPassword ? "text" : "password"}
+              autoComplete="current-password"
+              {...register("password", {
+                required: true,
+              })}
+            />
+          </InputGroup>
+          <FormErrorMessage>
+            {errors.password && errors.password.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button type="submit" colorScheme="blue" size="lg" fontSize="md">
+          Sign in
+        </Button>
+      </Stack>
+    </form>
   );
 };
 
